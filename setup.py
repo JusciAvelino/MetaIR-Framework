9e8b0da902140eb990540b86e318eb0da85d9ee6
Juscimara Avelino <64095923+JusciAvelino@users.noreply.github.com>
2025-11-11 23:57:42 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MetaIR-Framework",
+    version="0.1.0",
+    author="Juscimara Avelino",
+    description="A meta-learning framework for imbalanced regression recommendation",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "xgboost",
+        "seaborn",
+        "matplotlib"
+    ],
+    python_requires=">=3.8",
+)
